f24ac1970aa093b901e2e91cc6c018270c0a1b24
Charlie Tupman <charlie@liontreestudios.co.uk>
2020-05-15 17:56:11 +0100

    Add files via upload
    all built files for brewdog app

---
+++ precache-manifest.4fc1224cb0aaca38b075dad089e8f2ee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a6d23f7685c3bf223180ad6b9fcb05c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "66d509f54c37624888c7",
+    "url": "/static/css/main.1362d807.chunk.css"
+  },
+  {
+    "revision": "54ac04cc27800b907a27",
+    "url": "/static/js/2.94a11c9b.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.94a11c9b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66d509f54c37624888c7",
+    "url": "/static/js/main.d9de238c.chunk.js"
+  },
+  {
+    "revision": "a836663f7cbfb1e64305",
+    "url": "/static/js/runtime-main.7acd7713.js"
+  }
+]);
